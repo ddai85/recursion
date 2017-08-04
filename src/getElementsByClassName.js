fcab65d0c382a098fb543d6195544140c1cbf768
DanDai <ddai85@gmail.com>
2017-08-04 10:56:03 -0700

    Write recursive function to generate array of elements with specific class

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,30 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+
+  //use browser built in querySelectorAll to fetch each selector matching 'className'
+  var classNameList = [];
+
+  findChildren = function(childList){
+  	var childArray = Array.from(childList);
+  	for (var i = 0; i < childArray.length; i++){
+  		if (childArray[i].classList.contains(className)){
+  			classNameList.push(childArray[i]);
+  		}
+  		if (childArray[i].children){
+        findChildren(childArray[i].children);
+  	  }
+  	}
+  }
+  findChildren(document.documentElement.children);
+  //console.log(Array.from(document.getElementsByClassName(className)))
+  return classNameList;
+
+  //var selectorArray = document.querySelectorAll('.' + className);
+  //return Array.prototype.slice.apply(selectorArray);
+  
+
+  //once class is fetched, store element into array use .clone() method
+
+  //return array with array of elements
 };
